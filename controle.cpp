b9271975db53a74ccc1a9cf06be08221b3b66447
Marco <Marco@DESKTOP-DD1FOKL>
2026-03-18 21:12:56 -0300

    corrigido controle para aparecer menu triangulo em musicas

--- controle.cpp
+++ controle.cpp
@@ -42,10 +42,14 @@ extern int msgTimer;
 extern char caminhoXMLAtual[256];
 extern char linksAtuais[10][512];
 
+extern unsigned char* imgPreview;
+
 extern void preencherRoot();
 extern void preencherExplorerHome();
 extern void abrirMenuAudioOpcoes();
 extern void tratarSelecaoAudio(int op);
+extern void preencherMenuMusicas();
+extern void tocarMusicaNova(const char* path);
 
 void executarAcaoX() {}
 void executarAcaoBolinha() {}
@@ -55,7 +59,7 @@ void processarControles(uint32_t botoes, int32_t uId, OrbisImeDialogSetting* ime
         processarControlesEdicao(botoes);
     }
     else {
-        // --- D-PAD CIMA/BAIXO ---
+        // --- D-PAD CIMA/BAIXO --- 
         if (botoes & (ORBIS_PAD_BUTTON_DOWN | ORBIS_PAD_BUTTON_UP)) {
             if (cd <= 0) {
                 if (showOpcoes) {
@@ -86,19 +90,31 @@ void processarControles(uint32_t botoes, int32_t uId, OrbisImeDialogSetting* ime
             else { if (imgPreview) { stbi_image_free(imgPreview); imgPreview = NULL; } strcpy(ultimoJogoCarregado, ""); }
         }
 
-        // --- BOTÃO X (CONFIRMAR) ---
+        // --- BOTÃO X (CONFIRMAR) --- 
         if (botoes & ORBIS_PAD_BUTTON_CROSS) {
             if (!pCross) {
-                if (showOpcoes) {
-                    if (menuAtual == MENU_AUDIO_OPCOES) tratarSelecaoAudio(selAudioOpcao);
-                    else acaoArquivo(selOpcao);
+
+                // 1. Verificações Prioritárias de Menu Suspenso (Opções e Áudio)
+                if (menuAtual == MENU_AUDIO_OPCOES && showOpcoes) {
+                    tratarSelecaoAudio(selAudioOpcao); // Aciona o Parar Música / Ajustes
                 }
+                else if (menuAtual == MENU_EXPLORAR && showOpcoes) {
+                    acaoArquivo(selOpcao);
+                }
+
+                // 2. Navegação Padrão
                 else if (menuAtual == ROOT) {
                     if (sel == 0) carregarXML("/app0/assets/lista.xml");
                     else if (sel == 1) preencherMenuBaixar();
                     else if (sel == 2) preencherMenuEditar();
                     else if (sel == 3) preencherExplorerHome();
-                    else if (sel == 4) preencherMenuMusicas();
+                    else if (sel == 4) {
+                        if (imgPreview) { stbi_image_free(imgPreview); imgPreview = NULL; }
+                        strcpy(ultimoJogoCarregado, "");
+                        preencherMenuMusicas();
+                        sel = 0;
+                        off = 0;
+                    }
                     else if (sel == 5) {
                         menuAtual = MENU_NOTEPAD;
                         memset(bufferTecladoC, 0, sizeof(bufferTecladoC));
@@ -120,12 +136,21 @@ void processarControles(uint32_t botoes, int32_t uId, OrbisImeDialogSetting* ime
                 }
                 else if (menuAtual == JOGAR_XML && strcasecmp(nomes[sel], "sp") == 0) carregarXML("/app0/assets/sp.xml");
 
-                // --- NAVEGAÇÃO REPOSITÓRIOS / BAIXAR ---
+                // 3. EXECUÇÃO DE MÚSICA (ISOLADA)
+                else if (menuAtual == MENU_MUSICAS && !showOpcoes) {
+                    if (sel == 0) {
+                        tocarMusicaNova("PARADO");
+                    }
+                    else {
+                        char mPath[256];
+                        sprintf(mPath, "/data/HyperNeiva/Musicas/%s", nomes[sel]);
+                        tocarMusicaNova(mPath);
+                    }
+                }
+
                 else if (menuAtual == MENU_BAIXAR) {
                     if (sel == 0) preencherMenuRepositorios();
-                    else if (sel == 1) { // Lógica CAPAS Original
-                        memset(nomes, 0, sizeof(nomes)); strcpy(nomes[0], "RETROARCH"); totalItens = 1; menuAtual = MENU_CAPAS;
-                    }
+                    else if (sel == 1) { memset(nomes, 0, sizeof(nomes)); strcpy(nomes[0], "RETROARCH"); totalItens = 1; menuAtual = MENU_CAPAS; }
                 }
                 else if (menuAtual == MENU_BAIXAR_REPOS) {
                     if (sel == 0) listarXMLsRepositorio();
@@ -137,13 +162,8 @@ void processarControles(uint32_t botoes, int32_t uId, OrbisImeDialogSetting* ime
                     if (strcmp(nomes[0], "XML Vazio ou Invalido") != 0) mostrarLinksJogo(sel);
                 }
                 else if (menuAtual == MENU_BAIXAR_LINKS) {
-                    // DISPARA O DOWNLOAD REAL DO LINK SELECIONADO
-                    if (strcmp(nomes[0], "Nenhum link disponivel") != 0) {
-                        iniciarDownload(linksAtuais[sel]);
-                    }
+                    if (strcmp(nomes[0], "Nenhum link disponivel") != 0) iniciarDownload(linksAtuais[sel]);
                 }
-                // --- FIM NAVEGAÇÃO REPOSITÓRIOS ---
-
                 else if (menuAtual == MENU_CAPAS) { memset(nomes, 0, sizeof(nomes)); for (int i = 0; i < 5; i++) strcpy(nomes[i], listaConsoles[i].nome); totalItens = 5; menuAtual = MENU_CONSOLES; }
                 else if (menuAtual == MENU_CONSOLES) { consoleAtual = sel; acaoRede(NULL, true, false); }
                 else if (menuAtual == MENU_EDITAR) {
@@ -167,12 +187,11 @@ void processarControles(uint32_t botoes, int32_t uId, OrbisImeDialogSetting* ime
                         char nP[256]; sprintf(nP, "%s/%s", pathExplorar, pL); listarDiretorio(nP);
                     }
                 }
-                else if (menuAtual == MENU_MUSICAS) {
-                    if (sel == 0) tocarMusicaNova("PARADO");
-                    else { char mPath[256]; sprintf(mPath, "/data/HyperNeiva/Musicas/%s", nomes[sel]); tocarMusicaNova(mPath); }
-                }
 
-                if (!editMode && !showOpcoes && menuAtual != SCRAPER_LIST && menuAtual != JOGAR_XML && menuAtual != MENU_NOTEPAD) { sel = 0; off = 0; }
+                // O Reset da lista seguro
+                if (!editMode && !showOpcoes && menuAtual != SCRAPER_LIST && menuAtual != JOGAR_XML && menuAtual != MENU_NOTEPAD && menuAtual != MENU_MUSICAS && menuAtual != MENU_BAIXAR_LINKS) {
+                    sel = 0; off = 0;
+                }
                 executarAcaoX();
                 pCross = true;
             }
@@ -186,45 +205,64 @@ void processarControles(uint32_t botoes, int32_t uId, OrbisImeDialogSetting* ime
                     showOpcoes = false;
                     if (menuAtual == MENU_AUDIO_OPCOES) menuAtual = MENU_MUSICAS;
                 }
-                else if (menuAtual == MENU_NOTEPAD) preencherRoot();
-                else if (menuAtual == JOGAR_XML) { if (strstr(xmlCaminhoAtual, "sp.xml")) carregarXML("/app0/assets/lista.xml"); else preencherRoot(); }
-                else if (menuAtual == MENU_BAIXAR || menuAtual == MENU_EDITAR || menuAtual == MENU_EXPLORAR_HOME || menuAtual == MENU_MUSICAS) preencherRoot();
+                else {
+                    if (menuAtual == MENU_NOTEPAD) preencherRoot();
+                    else if (menuAtual == JOGAR_XML) { if (strstr(xmlCaminhoAtual, "sp.xml")) carregarXML("/app0/assets/lista.xml"); else preencherRoot(); }
+                    else if (menuAtual == MENU_BAIXAR || menuAtual == MENU_EDITAR || menuAtual == MENU_EXPLORAR_HOME || menuAtual == MENU_MUSICAS) preencherRoot();
+                    else if (menuAtual == MENU_BAIXAR_REPOS) preencherMenuBaixar();
+                    else if (menuAtual == MENU_BAIXAR_GAMES_XMLS) preencherMenuRepositorios();
+                    else if (menuAtual == MENU_BAIXAR_GAMES_LIST) listarXMLsRepositorio();
+                    else if (menuAtual == MENU_BAIXAR_LINKS) {
+                        char nomeXML[256]; char* ultimaBarra = strrchr(caminhoXMLAtual, '/');
+                        if (ultimaBarra) strcpy(nomeXML, ultimaBarra + 1);
+                        abrirXMLRepositorio(nomeXML);
+                    }
+                    else if (menuAtual == MENU_CAPAS) preencherMenuBaixar();
+                    else if (menuAtual == MENU_CONSOLES) { memset(nomes, 0, sizeof(nomes)); strcpy(nomes[0], "RETROARCH"); totalItens = 1; menuAtual = MENU_CAPAS; }
+                    else if (menuAtual == SCRAPER_LIST) { memset(nomes, 0, sizeof(nomes)); for (int i = 0; i < 5; i++) strcpy(nomes[i], listaConsoles[i].nome); totalItens = 5; menuAtual = MENU_CONSOLES; }
+                    else if (menuAtual == MENU_EDIT_TARGET) preencherMenuEditar();
+                    else if (menuAtual == MENU_EXPLORAR) {
+                        if (strcmp(pathExplorar, baseRaiz) == 0) preencherExplorerHome();
+                        else { char* last = strrchr(pathExplorar, '/'); if (last) { if (last == pathExplorar) strcpy(pathExplorar, "/"); else *last = '\0'; listarDiretorio(pathExplorar); } }
+                    }
 
-                // Voltar nos Repositórios
-                else if (menuAtual == MENU_BAIXAR_REPOS) preencherMenuBaixar();
-                else if (menuAtual == MENU_BAIXAR_GAMES_XMLS) preencherMenuRepositorios();
-                else if (menuAtual == MENU_BAIXAR_GAMES_LIST) listarXMLsRepositorio();
-                else if (menuAtual == MENU_BAIXAR_LINKS) {
-                    char nomeXML[256];
-                    char* ultimaBarra = strrchr(caminhoXMLAtual, '/');
-                    if (ultimaBarra) strcpy(nomeXML, ultimaBarra + 1);
-                    abrirXMLRepositorio(nomeXML);
+                    if (menuAtual != MENU_AUDIO_OPCOES) { sel = 0; off = 0; }
                 }
-
-                else if (menuAtual == MENU_CAPAS) preencherMenuBaixar();
-                else if (menuAtual == MENU_CONSOLES) { memset(nomes, 0, sizeof(nomes)); strcpy(nomes[0], "RETROARCH"); totalItens = 1; menuAtual = MENU_CAPAS; }
-                else if (menuAtual == SCRAPER_LIST) { memset(nomes, 0, sizeof(nomes)); for (int i = 0; i < 5; i++) strcpy(nomes[i], listaConsoles[i].nome); totalItens = 5; menuAtual = MENU_CONSOLES; }
-                else if (menuAtual == MENU_EDIT_TARGET) preencherMenuEditar();
-                else if (menuAtual == MENU_EXPLORAR) {
-                    if (strcmp(pathExplorar, baseRaiz) == 0) preencherExplorerHome();
-                    else { char* last = strrchr(pathExplorar, '/'); if (last) { if (last == pathExplorar) strcpy(pathExplorar, "/"); else *last = '\0'; listarDiretorio(pathExplorar); } }
-                }
-                if (!showOpcoes) { sel = 0; off = 0; }
                 executarAcaoBolinha();
                 pCircle = true;
             }
         }
         else pCircle = false;
 
-        // --- BOTÃO TRIÂNGULO / R1 ---
-        if (menuAtual == MENU_EXPLORAR) {
-            if (botoes & ORBIS_PAD_BUTTON_R1) { if (cd <= 0) { marcados[sel] = !marcados[sel]; cd = 12; } }
-            if (botoes & ORBIS_PAD_BUTTON_TRIANGLE) { if (!pTri) { showOpcoes = !showOpcoes; selOpcao = 0; pTri = true; } }
-            else pTri = false;
+        // --- BOTÃO TRIÂNGULO E R1 ---
+        if (botoes & ORBIS_PAD_BUTTON_TRIANGLE) {
+            if (!pTri) {
+                if (menuAtual == MENU_EXPLORAR) {
+                    showOpcoes = !showOpcoes;
+                    selOpcao = 0;
+                }
+                else if (menuAtual == MENU_MUSICAS) {
+                    abrirMenuAudioOpcoes();
+                    menuAtual = MENU_AUDIO_OPCOES;
+                    showOpcoes = true;
+                    selAudioOpcao = 0;
+                }
+                pTri = true;
+            }
         }
-        else if (menuAtual == MENU_MUSICAS) {
-            if (botoes & ORBIS_PAD_BUTTON_TRIANGLE) { if (!pTri) { abrirMenuAudioOpcoes(); pTri = true; } }
-            else pTri = false;
+        else {
+            pTri = false;
+        }
+
+        if (botoes & ORBIS_PAD_BUTTON_R1) {
+            if (menuAtual == MENU_EXPLORAR) {
+                if (cd <= 0) { marcados[sel] = !marcados[sel]; cd = 12; }
+            }
+        }
+
+        // --- PROTEÇÃO DE ESTADO VISUAL ---
+        if (menuAtual != MENU_EXPLORAR && menuAtual != MENU_AUDIO_OPCOES) {
+            showOpcoes = false;
         }
     }
 }
