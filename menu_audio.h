cbc96c64d9c0568a208c806327d214905814efe8
Marco <Marco@DESKTOP-DD1FOKL>
2026-03-21 08:07:47 -0300

    audio proximo

--- menu_audio.h
+++ menu_audio.h
@@ -3,7 +3,13 @@
 #define MENU_AUDIO_H
 
 #include <stdint.h>
+#include "menu.h" // Isso já traz a sua lista (enum) completa e oficial do sistema!
+
+// Disponibiliza a lista de nomes para quem quiser usar
+extern const char* listaOpcoesAudio[11];
 
 void desenharMenuAudio(uint32_t* p);
+void abrirMenuAudioOpcoes();
+void tratarSelecaoAudio(int op);
 
 #endif
